06c70ef13ecf033cd51536aae97242b00a3e6ea3
Lokijopu <kguo2134@gmail.com>
2019-05-31 13:36:42 -0400

    Add files via upload

--- src/Mancala.java
+++ src/Mancala.java
@@ -1,7 +1,14 @@
 import java.awt.CardLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,6 +16,8 @@ import javax.swing.JPanel;
 
 public class Mancala extends JFrame{
 	private int numTurns;
+	private int redPoints;
+	private int bluePoints;
 	public Mancala() {
 		setTitle("Mancala");
 		numTurns = 0;
@@ -23,18 +32,64 @@ public class Mancala extends JFrame{
 		
 		JPanel panel1 = new JPanel();
 		JLabel mancalaLabel = new JLabel("Mancala");
-		JButton button1 = new JButton("Start");
+		JButton start = new JButton("Start");
 		panel1.add(mancalaLabel);
-		panel1.add(button1);
+		panel1.add(start);
+		start.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				cL.show(overall, "panel 2");
+				
+			}
+			
+		});
 		JPanel panel2 = new JPanel();
+		JButton play = new JButton("Play");
+		panel2.add(play);
+		play.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				cL.show(overall, "panel 3");
+				
+			}
+			
+		});
 		
 		JPanel panel3 = new JPanel();
+		JButton home = new JButton("Home");
+		panel3.add(home);
+		home.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				cL.show(overall, "panel 1");
+				
+			}
+			
+		});
+		BufferedImage buttonIcon = ImageIO.read(new File("Mancala Board.jfif"));
+		JButton button = new JButton(new ImageIcon(buttonIcon));
+		button.setBorder(BorderFactory.createEmptyBorder());
+		button.setContentAreaFilled(false);
 		
 		overall.add(panel1, "panel 1");
 		overall.add(panel2, "panel 2");
 		overall.add(panel3, "panel 3");
+		
+		cL.show(overall, "panel1");
+		add(overall);
 	}
-	
+	public void resetBoard() {
+		
+	}
+	public void moveGem() {
+		
+	}
+	public void getNumGems() {
+		
+	}
 	public static void main (String[] args) {
 		new Mancala();
 	}
